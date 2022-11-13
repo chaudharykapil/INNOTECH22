7d5d3eab3cb52e6b1891cda53973ca961370576b
Garvit Singhal <userunknown3985@gmail.com>
2022-11-13 22:47:30 +0530

    initial

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+    # manageinstance = Manager(
+    #     latitude=latitude, longitude=longitude, time=datetime.now(),name=location)
+    
+
+    # db.session.add(manageinstance)
+    # db.session.commit()
+    
