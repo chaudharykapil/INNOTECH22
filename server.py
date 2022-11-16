08a8f03e9a9c9694ca7153c136a4d37130be1294
chaudharykapil <chaudharykapil8279@gmail.com>
2022-11-17 01:50:29 +0530

    add

--- server.py
+++ server.py
@@ -14,23 +14,38 @@ app.app_context().push()
 
 app.secret_key = "super secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://kapil829:ashish1234#@kapil829.mysql.pythonanywhere-services.com/kapil829$innotech'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kapil@localhost/innotech'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-api_url = "http://api.positionstack.com/v1/reverse?access_key=65aa3458e8ba59795182408ec196f7af&query=28.693138,77.533440"
-#api_url = "http://api.positionstack.com/v1/forward? access_key = 65aa3458e8ba59795182408ec196f7af& query = 1600 Pennsylvania Ave NW, Washington DC"
-@app.route('/')
+
+@app.route("/")
+def Home():
+    data = Manager.query.all()
+    db.session.close()
+    print(data)
+    return render_template("Maintable.html",data = data)
+
+
+@app.route('/api/sendcoord')
 def  coordinate():
     #res = requests.get(api_url)
     latlong = request.args.get('latlong')
     if(latlong and len(latlong) > 0):
         coord = latlong.split(" ")
-        manger = Manager(
-            Latitude = coord[0],
-            Longitude = coord[1],
-            time = str(datetime.now()),
-            name = ""
-            )
-        db.session.add(manger)
-        db.session.commit()
-        return "1"
+        if(coord and len(coord)>=2):
+            manger = Manager(
+                Latitude = coord[0],
+                Longitude = coord[1],
+                time = str(datetime.now()),
+                name = ""
+                )
+            db.session.add(manger)
+            db.session.commit()
+            db.session.close()
+            return "1"
+        return "0"
     return "0"
+
+#app.run(debug=True)
